e5f7592191651b50361eb0a01ed118336363a23f
oerts <github@ogas.anonaddy.com>
2021-03-21 14:14:26 +0300

    skeleton html & css

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let library = [];
 
-function Book (title, author, pages, read) {
+function Book (title, author, category, read) {
     this.title = title
     this.author = author
-    this.pages = pages
+    this.category = category
     this.read = read
 }
 
-function addBook (bookTitle, bookAuthor, bookPages, bookCondition) {
-    let newBook = new Book (bookTitle, bookAuthor, bookPages, bookCondition);
+function addBook (bookTitle, bookAuthor, bookCategory, bookCondition) {
+    let newBook = new Book (bookTitle, bookAuthor, bookCategory, bookCondition);
     library.push(newBook);
+
+}
+
+function generateBooks(library) {
+    for (let i = 0; i < library.length; i++) {
+        
+    }
 }
